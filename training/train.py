8d97d4e92761a7fbf9c997ff1b74d0bd76758a37
Ojus Mohan <ojus001@narval2.narval.calcul.quebec>
2025-05-30 02:02:13 -0400

    updated training loop code in train.py

--- training/train.py
+++ training/train.py
@@ -4,7 +4,9 @@ import torchvision
 import torchvision.transforms as transforms
 import torchvision.models as models
 import torch.nn as nn
+import torch.optim as optim
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 # from utils import train_model  # assuming you have this in utils.py
 
 def load_data(is_train = True, batch_size: int = 64) -> DataLoader:
@@ -43,13 +45,70 @@ def load_data(is_train = True, batch_size: int = 64) -> DataLoader:
     
     return data_loader
 
+# Training Loop
+def train_model(model, train_loader, test_loader, criterion, optimizer, num_epochs=10, device = "cpu"):
+    best_acc = 0.0
+    
+    for epoch in range(num_epochs):
+        model.train() # Sets the model to training mode
+        running_loss = 0.0
+        correct = 0
+        total = 0
+        
+        loop = tqdm(train_loader, desc=f"Epoch [{epoch+1}/{num_epochs}]") # ?
+        
+        # Standard PyTorch training step for each batch: 
+        #   forward pass → compute loss → backprop → update weights.
+        for images, labels in loop: # Each batch is fetched from the loader
+            optimizer.zero_grad() #  clears old gradients from the previous step, so they don't accumulate during backpropagation.
+            outputs = model(images)
+            print(f'Output Shape:{outputs.shape}')
+            loss = criterion(outputs, labels)
+            loss.backward()
+            optimizer.step()
+            
+            # Track number of correctly predicted samples for accuracy.
+            _, predicted = torch.max(outputs.data, 1) # outputs contains raw scores (logits) for each class.
+            correct += (predicted == labels).sum().item()
+            
+            # Progress Display:
+            loop.set_postfix(loss=loss.item(), acc=100*correct/total)
+            
+        # Validation
+        val_acc = evaluate(model, test_loader, device)
+        print(f"\nValidation Accuracy after Epoch {epoch+1}: {val_acc:.2f}%")
+        
+                # Save the best model
+        if val_acc > best_acc:
+            best_acc = val_acc
+            torch.save(model.state_dict(), 'best_model.pth')
+            print("Best model saved.")
+
+# Evaluation function
+def evaluate(model, dataloader, device='cpu'):
+    model.eval()
+    correct = 0
+    total = 0
+
+    with torch.no_grad():
+        for images, labels in dataloader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    return 100 * correct / total
+
 def main():
     """
     Main function to load data, initialize model, and start training.
     """
+    print('1. Data Loading...')
     train_loader = load_data(is_train=True, batch_size=64)  # Load training data
     test_loader = load_data(is_train=False, batch_size=256)  # Load testing data
 
+    print('2.Model Loading...')
     ## Model Loading
     # Load pretrained RESNET-50 model
     model = models.resnet50(weights=models.ResNet50_Weights.DEFAULT)
@@ -70,8 +129,13 @@ def main():
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
     
-    ## Call your training loop
-    # train_model(model, train_loader, device=device)
+    # Define Loss and Optimizer
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.fc.parameters(), lr=0.001) # only update classifier layer (final fc layer)
+    
+    print('3. Training...')
+    ## Call your training loop    
+    train_model(model, train_loader, test_loader, criterion, optimizer, num_epochs=10, device=device)
 
 if __name__ == "__main__":
     main()
