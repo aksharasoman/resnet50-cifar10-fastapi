6e369efb08764bc2a54e0922ab4113056cc2daaa
aksharasoman <aksharasoman@gmail.com>
2025-06-09 02:40:36 +0530

    edited gpu.sh

--- training/gpu_job.sh
+++ training/gpu_job.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-#SBATCH --account=def-someuser
+#SBATCH --account=def-mushrifs
 #SBATCH --gpus-per-node=1
 #SBATCH --mem=4000M               # memory per node
 #SBATCH --time=0-03:00
 module load python/3.12 
 module load scipy-stack
 source ~/.venvs/venv/bin/activate
-python train_classConditionalAugmentation.py
+python train_SGDoptimizer.py
