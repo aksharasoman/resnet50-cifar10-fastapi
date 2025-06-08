162f76111fb7a4d5842e92655fc3875849a5675d
aksharasoman <aksharasoman@gmail.com>
2025-06-09 02:37:01 +0530

    added sbatch submission sh script

---
+++ training/gpu_job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --account=def-someuser
+#SBATCH --gpus-per-node=1
+#SBATCH --mem=4000M               # memory per node
+#SBATCH --time=0-03:00
+module load python/3.12 
+module load scipy-stack
+source ~/.venvs/venv/bin/activate
+python train_SGDoptimizer.py
